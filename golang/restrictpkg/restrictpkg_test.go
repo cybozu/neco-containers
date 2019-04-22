5ab84e933466d85f29a23a878fd9edf680d9db49
dulltz <isrgnoe@gmail.com>
2019-04-22 18:54:40 +0900

    [golang] add source code of restrictpkg

---
+++ golang/restrictpkg/restrictpkg_test.go
@@ -0,0 +1,18 @@
+package restrictpkg_test
+
+import (
+	"testing"
+
+	"github.com/cybozu/neco-containers/golang/restrictpkg"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+func Test(t *testing.T) {
+	testdata := analysistest.TestData()
+	analyzer := restrictpkg.RestrictPackageAnalyzer
+	err := analyzer.Flags.Set("packages", "html/template")
+	if err != nil {
+		panic(err)
+	}
+	analysistest.Run(t, testdata, analyzer, "a", "b")
+}
