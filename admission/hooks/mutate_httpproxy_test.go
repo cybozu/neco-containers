dc87e9901d6c6ad15af4355749b822fea1263438
Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>
2019-12-19 07:23:19 +0000

    Add tests for MutateHttpProxy Webhook
    Signed-off-by: Yusuke Suzuki <yusuke-suzuki@cybozu.co.jp>

---
+++ admission/hooks/mutate_httpproxy_test.go
@@ -0,0 +1,44 @@
+package hooks
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	contourv1 "github.com/projectcontour/contour/apis/projectcontour/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func testHTTPProxy(name string, annotations map[string]string) *contourv1.HTTPProxy {
+	hp := &contourv1.HTTPProxy{}
+	hp.Name = name
+	hp.Namespace = "default"
+	hp.Annotations = annotations
+
+	err := k8sClient.Create(testCtx, hp)
+	Expect(err).NotTo(HaveOccurred())
+
+	ret := &contourv1.HTTPProxy{}
+	err = k8sClient.Get(testCtx, types.NamespacedName{Name: name, Namespace: "default"}, ret)
+	Expect(err).NotTo(HaveOccurred())
+
+	return ret
+}
+
+var _ = Describe("mutate HTTPProxy webhook", func() {
+	It("should have default annotation", func() {
+		hp := testHTTPProxy("hp1", map[string]string{})
+		Expect(hp.Annotations).To(HaveKeyWithValue(annotationKubernetesIngressClass, annotationIngressClassDefault))
+		Expect(hp.Annotations).ToNot(HaveKey(annotationContourIngressClass))
+	})
+
+	It("should not mutate annotations", func() {
+		hp := testHTTPProxy("hp2", map[string]string{annotationKubernetesIngressClass: "global"})
+		Expect(hp.Annotations).To(HaveKeyWithValue(annotationKubernetesIngressClass, "global"))
+		Expect(hp.Annotations).ToNot(HaveKey(annotationContourIngressClass))
+	})
+
+	It("should not mutate annotations with projectcontour.io/ingress.class", func() {
+		hp := testHTTPProxy("hp3", map[string]string{annotationContourIngressClass: "global"})
+		Expect(hp.Annotations).To(HaveKeyWithValue(annotationContourIngressClass, "global"))
+		Expect(hp.Annotations).ToNot(HaveKey(annotationKubernetesIngressClass))
+	})
+})
