09e918bf45daa943e88a7646f27f22121bd6dfb9
Yuji Ito <llamerada.jp@gmail.com>
2020-01-06 06:46:56 +0000

    Use CreateOrUpdate correctly.
    Signed-off-by: Yuji Ito <llamerada.jp@gmail.com>

--- local-pv-provisioner/controllers/persistent_volume.go
+++ local-pv-provisioner/controllers/persistent_volume.go
@@ -48,29 +48,24 @@ func (dd *DeviceDetector) pvName(devName string) string {
 func (dd *DeviceDetector) createPV(ctx context.Context, dev Device, node *corev1.Node) error {
 	pvMode := corev1.PersistentVolumeBlock
 	log := dd.log.WithValues("node", dd.nodeName)
-	pv := &corev1.PersistentVolume{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: dd.pvName(dev.Path),
-		},
-		Spec: corev1.PersistentVolumeSpec{
-			Capacity: corev1.ResourceList{
-				corev1.ResourceStorage: *resource.NewQuantity(dev.CapacityBytes, resource.BinarySI),
-			},
-			VolumeMode:                    &pvMode,
-			AccessModes:                   []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
-			PersistentVolumeReclaimPolicy: corev1.PersistentVolumeReclaimRetain,
-			StorageClassName:              storageClass,
-			PersistentVolumeSource:        corev1.PersistentVolumeSource{Local: &corev1.LocalVolumeSource{Path: dev.Path}},
-			NodeAffinity: &corev1.VolumeNodeAffinity{
-				Required: &corev1.NodeSelector{NodeSelectorTerms: []corev1.NodeSelectorTerm{
-					{MatchExpressions: []corev1.NodeSelectorRequirement{
-						{Key: hostNameLabelKey, Operator: "In", Values: []string{dd.nodeName}},
-					}},
-				}},
-			},
-		},
-	}
+	pv := &corev1.PersistentVolume{ObjectMeta: metav1.ObjectMeta{Name: dd.pvName(dev.Path)}}
+
 	op, err := ctrl.CreateOrUpdate(ctx, dd.Client, pv, func() error {
+		pv.Spec.AccessModes = []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce}
+		pv.Spec.Capacity = corev1.ResourceList{
+			corev1.ResourceStorage: *resource.NewQuantity(dev.CapacityBytes, resource.BinarySI),
+		}
+		pv.Spec.NodeAffinity = &corev1.VolumeNodeAffinity{
+			Required: &corev1.NodeSelector{NodeSelectorTerms: []corev1.NodeSelectorTerm{
+				{MatchExpressions: []corev1.NodeSelectorRequirement{
+					{Key: hostNameLabelKey, Operator: "In", Values: []string{dd.nodeName}},
+				}},
+			}},
+		}
+		pv.Spec.PersistentVolumeReclaimPolicy = corev1.PersistentVolumeReclaimRetain
+		pv.Spec.PersistentVolumeSource = corev1.PersistentVolumeSource{Local: &corev1.LocalVolumeSource{Path: dev.Path}}
+		pv.Spec.StorageClassName = storageClass
+		pv.Spec.VolumeMode = &pvMode
 		return ctrl.SetControllerReference(node, pv, dd.scheme)
 	})
 	if err != nil {
