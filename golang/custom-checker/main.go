48becd214043643f54bf089ac1fea4e4fc11edcd
dulltz <isrgnoe@gmail.com>
2020-02-26 16:29:52 +0900

    golang: add custom-checker
    Signed-off-by: dulltz <isrgnoe@gmail.com>

--- golang/custom-checker/main.go
+++ golang/custom-checker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/cybozu/neco-containers/golang/eventuallycheck"
 	"github.com/cybozu/neco-containers/golang/restrictpkg"
 	"golang.org/x/tools/go/analysis/multichecker"
 )
@@ -8,6 +9,6 @@ import (
 func main() {
 	multichecker.Main(
 		restrictpkg.RestrictPackageAnalyzer,
-		//eventuallycheck.Analyzer,
+		eventuallycheck.Analyzer,
 	)
 }
