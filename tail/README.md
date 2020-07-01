b3d3bd4fd3f9dde18c8cd8e0b30c530551e0b1e8
zoetrope <a.ikezoe@gmail.com>
2020-07-01 10:43:55 +0900

    add tail container

---
+++ tail/README.md
@@ -0,0 +1,14 @@
+# tail
+
+
+[tail](https://github.com/nxadm/tail)
+
+## Usage
+
+To launch server with specific config file.
+
+    $ docker run quay.io/cybozu/tail:1.4 -- -f /path/to/logfile
+ 
+## Docker images
+
+Docker images are available on [Quay.io](https://quay.io/repository/cybozu/tail)
---
+++ tail/src/tail/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/cybozu-go/log"
+	"github.com/cybozu-go/well"
+	"github.com/nxadm/tail"
+)
+
+func main() {
+	follow := flag.Bool("f", false, "follow the growth of the file")
+	flag.Parse()
+
+	fmt.Printf("follow: %v\n", *follow)
+	if flag.NArg() < 1 {
+		fmt.Println("usage: ./tail [-f] [FILE]...")
+		os.Exit(1)
+	}
+
+	for _, file := range flag.Args() {
+		file := file
+		well.Go(func(ctx context.Context) error {
+			t, err := tail.TailFile(file, tail.Config{Follow: *follow})
+			if err != nil {
+				return err
+			}
+			go func(){
+				<- ctx.Done()
+				t.Stop()
+			}()
+			for line := range t.Lines {
+				fmt.Println(line.Text)
+			}
+			return nil
+		})
+	}
+	well.Stop()
+	err := well.Wait()
+	if err != nil && !well.IsSignaled(err) {
+		log.ErrorExit(err)
+	}
+}
