cbb387147c2604ea9f75edc50409ab1fd8f2aec3
Hiroshi Muraoka <h.muraoka714@gmail.com>
2021-03-02 10:52:17 +0900

    actions-runner: Update entrypoint to manipulate runner instead pod
    Signed-off-by: Hiroshi Muraoka <h.muraoka714@gmail.com>

--- actions-runner/entrypoint_wrapper.sh
+++ actions-runner/entrypoint_wrapper.sh
@@ -7,21 +7,21 @@ echo "Run entrypoint"
 ./entrypoint.sh
 
 if [ -f /home/runner/delete-immediately ]; then
-    echo "Label pod with current time"
-    kubectl label pod ${RUNNER_NAME} delete-at=$(date +%Y%m%d%H%M%S)
+    echo "Label runners with current time"
+    kubectl label runners.actions.summerwind.dev ${RUNNER_NAME} delete-at=$(date +%Y%m%d%H%M%S)
 else
-    echo "Label pod with current time + 1m"
-    kubectl label pod ${RUNNER_NAME} delete-at=$(date -d "1 minutes" +%Y%m%d%H%M%S)
+    echo "Label runners with current time + 1m"
+    kubectl label runners.actions.summerwind.dev ${RUNNER_NAME} delete-at=$(date -d "1 minutes" +%Y%m%d%H%M%S)
 fi
 
 echo "Wait until delete-at"
 while true
 do
-    DELETE_AT=$(kubectl get po ${RUNNER_NAME} -o jsonpath='{.metadata.labels.delete-at}')
+    DELETE_AT=$(kubectl get runners.actions.summerwind.dev ${RUNNER_NAME} -o jsonpath='{.metadata.labels.delete-at}')
     NOW=$(date +%Y%m%d%H%M%S)
     if [ -n "${DELETE_AT}" ] && [ ${NOW} -gt ${DELETE_AT} ]; then
         echo "Delete ${RUNNER_NAME}"
-        kubectl delete po ${RUNNER_NAME}
+        kubectl delete runners.actions.summerwind.dev ${RUNNER_NAME}
     fi
     echo "sleeping..."
     sleep 30
