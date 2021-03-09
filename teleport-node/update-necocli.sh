74d5f17c13d79e444da6cfdbf7d8a03e295daa29
Kazuhito MATSUDA <kazuhito.matuda@gmail.com>
2021-03-09 13:59:47 +0900

    Fix and reflect comments.
    Signed-off-by: Kazuhito MATSUDA <kazuhito.matuda@gmail.com>

--- teleport-node/update-necocli.sh
+++ teleport-node/update-necocli.sh
@@ -5,13 +5,16 @@ export HTTPS_PROXY="http://squid.internet-egress.svc:3128"
 DATE=$(curl -s "https://api.github.com/repos/cybozu-go/neco/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' | sed -e "s/release-//")
 FILE="neco-operation-cli-linux_${DATE}_amd64.deb"
 
-ADD_PATH="${HOME}/neco-operation-cli/usr/bin"
-if [ ! -f "${HOME}/.profile" ]  || [ ! "$(cat ${HOME}/.profile | grep ${ADD_PATH})" ]; then
+CLI_PATH="${HOME}/neco-operation-cli/usr/bin"
+if [ ! -f "${HOME}/.profile" ]  || [ ! "$(cat ${HOME}/.profile | grep ${CLI_PATH})" ]; then
     echo "export PATH=${PATH}:${HOME}/neco-operation-cli/usr/bin" >> ${HOME}/.profile
 fi
 
 if [ ! -f $FILE ]; then
+    echo "Downloading and extracting Neco CLI tools..."
     curl -sLf -O https://github.com/cybozu-go/neco/releases/download/release-${DATE}/neco-operation-cli-linux_${DATE}_amd64.deb
     mkdir -p ${HOME}/neco-operation-cli
     dpkg -x $FILE ${HOME}/neco-operation-cli
+    mkdir -p ${HOME}/deb
+    mv ${FILE} ${HOME}/deb/
 fi
