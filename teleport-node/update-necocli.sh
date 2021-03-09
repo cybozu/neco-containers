8262f4f4030a7c9bf2aeaa2d0702d2dd2a00f934
Kazuhito MATSUDA <kazuhito.matuda@gmail.com>
2021-03-09 15:56:56 +0900

    Fix.
    Signed-off-by: Kazuhito MATSUDA <kazuhito.matuda@gmail.com>

--- teleport-node/update-necocli.sh
+++ teleport-node/update-necocli.sh
@@ -1,14 +1,18 @@
 #! /bin/sh -ex
 
-export HTTPS_PROXY="http://squid.internet-egress.svc:3128"
+export KUBERNETES_SERVICE_PORT_HTTPS="443"
+export KUBERNETES_SERVICE_PORT="443"
+export KUBERNETES_PORT_443_TCP="tcp://kubernetes.default.svc:443"
+export KUBERNETES_PORT_443_TCP_PROTO="tcp"
+export KUBERNETES_PORT_443_TCP_ADDR="kubernetes.default.svc"
+export KUBERNETES_SERVICE_HOST="kubernetes.default.svc"
+export KUBERNETES_PORT="tcp://kubernetes.default.svc:443"
+export KUBERNETES_PORT_443_TCP_PORT="443"
+export PATH="${PATH}:${HOME}/neco-operation-cli/usr/bin"
 
+export HTTPS_PROXY="http://squid.internet-egress.svc:3128"
 DATE=$(curl -s "https://api.github.com/repos/cybozu-go/neco/releases/latest" | jq -r ".tag_name" | sed -e "s/release-//")
 FILE="neco-operation-cli-linux_${DATE}_amd64.deb"
-
-CLI_PATH="${HOME}/neco-operation-cli/usr/bin"
-if [ ! -f "${HOME}/.profile" ]  || [ ! "$(cat ${HOME}/.profile | grep ${CLI_PATH})" ]; then
-    echo "export PATH=\${PATH}:${HOME}/neco-operation-cli/usr/bin" >> ${HOME}/.profile
-fi
 
 if [ ! -f ${HOME}/deb/${FILE} ]; then
     echo "Downloading and extracting Neco CLI tools..."
@@ -18,3 +22,5 @@ if [ ! -f ${HOME}/deb/${FILE} ]; then
     mkdir -p ${HOME}/deb
     mv ${FILE} ${HOME}/deb/
 fi
+
+unset HTTPS_PROXY
