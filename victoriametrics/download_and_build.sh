2e404d6f2cdc4ba75dc33ca9662257d329d0467f
UMEZAWA Takeshi <takeshi-umezawa@cybozu.co.jp>
2024-04-19 15:38:14 +0900

    victoriametrics: vmagent: add scrape_body_size_bytes auto metrics
    Signed-off-by: UMEZAWA Takeshi <takeshi-umezawa@cybozu.co.jp>

--- victoriametrics/download_and_build.sh
+++ victoriametrics/download_and_build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -eo pipefail
-curl -fsSL -o victoriametrics.tar.gz "https://github.com/${VICTORIAMETRICS_SRCREPO}/archive/v${VICTORIAMETRICS_VERSION}.tar.gz" \
-    && tar -x -z --strip-components 1 -f victoriametrics.tar.gz \
-    && rm -f victoriametrics.tar.gz \
-    && BUILDINFO_TAG=v${VICTORIAMETRICS_VERSION} PKG_TAG=v${VICTORIAMETRICS_VERSION} make "$@"
+curl -fsSL -o victoriametrics.tar.gz "https://github.com/${VICTORIAMETRICS_SRCREPO}/archive/v${VICTORIAMETRICS_VERSION}.tar.gz"
+tar -x -z --strip-components 1 -f victoriametrics.tar.gz
+rm -f victoriametrics.tar.gz
+
+for P in /*.patch; do
+    if [ -f "$P" ]; then
+        patch -p1 < $P
+    fi
+done
+
+BUILDINFO_TAG=v${VICTORIAMETRICS_VERSION} PKG_TAG=v${VICTORIAMETRICS_VERSION} make "$@"
