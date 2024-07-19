a99567637f081dfe3a35239d2351b007b4009fd6
Maho Takara <takara@cybozu.co.jp>
2024-07-19 01:58:20 +0000

    update

--- bmc-log-collector/log-collector2_test.go
+++ bmc-log-collector/log-collector2_test.go
@@ -36,8 +36,8 @@ var _ = Describe("Collecting by parallel workers", Ordered, func() {
 		os.Remove("testdata/pointers/HN3CLP3")
 		os.Remove("testdata/output/HN3CLP3")
 
-		//os.Remove("testdata/pointers/J7N6MW3")
-		//os.Remove("testdata/output/J7N6MW3")
+		os.Remove("testdata/pointers/J7N6MW3")
+		os.Remove("testdata/output/J7N6MW3")
 
 		ctx, cancel := context.WithCancel(context.Background())
 		mq := Queue{
@@ -45,16 +45,16 @@ var _ = Describe("Collecting by parallel workers", Ordered, func() {
 			mu:    &m,
 		}
 		lc = logCollector{
-			machinesPath: "testdata/configmap/serverlist.csv",
+			machinesPath: "testdata/configmap/serverlist2.csv",
 			miniNum:      1,  // 最小
 			maxiNum:      10, // 最大
-			currNum:      2,  // 決定コレクター数
+			currNum:      1,  // 決定コレクター数
 			rfUrl:        "/redfish/v1/Managers/iDRAC.Embedded.1/LogServices/Sel/Entries",
 			ptrDir:       "testdata/pointers",
 			ctx:          ctx, // コンテキスト
 			cancel:       cancel,
 			que:          mq, // コレクターのキュー
-			interval:     20, // 待機秒数
+			interval:     3,  // 待機秒数
 			wg:           &wg,
 			testMode:     true,
 			testOut:      "testdata/output",
@@ -76,13 +76,14 @@ var _ = Describe("Collecting by parallel workers", Ordered, func() {
 		}
 		bm2.startMock()
 
-		//bm3 := bmcMock{
-		//	host:   "127.0.0.1:7380",
-		//	resDir: "testdata/redfish_response",
-		//	files:  []string{"J7N6MW3-1.json", "J7N6MW3-2.json", "J7N6MW3-3.json"},
-		//}
-		//bm3.startMock()
+		bm3 := bmcMock{
+			host:   "127.0.0.1:7380",
+			resDir: "testdata/redfish_response",
+			files:  []string{"J7N6MW3-1.json", "J7N6MW3-2.json", "J7N6MW3-3.json"},
+		}
+		bm3.startMock()
 
+		// Wait starting stub servers
 		time.Sleep(10 * time.Second)
 	})
 	BeforeEach(func() {
@@ -94,30 +95,36 @@ var _ = Describe("Collecting by parallel workers", Ordered, func() {
 		var machinesList Machines
 		var err error
 
-		/*
-			var rslt1, rslt2, rslt3 SystemEventLog
-			var serial1 string = "683FPQ3"
-			var serial2 string = "HN3CLP3"
-			var serial3 string = "J7N6MW3"
-			var file1, file2, file3 *os.File
-			var reader1, reader2, reader3 *bufio.Reader
-		*/
-
-		var rslt1, rslt2 SystemEventLog
+		var rslt1, rslt2, rslt3 SystemEventLog
 		var serial1 string = "683FPQ3"
 		var serial2 string = "HN3CLP3"
-		var file1, file2 *os.File
-		var reader1, reader2 *bufio.Reader
+		var serial3 string = "J7N6MW3"
+		var file1, file2, file3 *os.File
+		var reader1, reader2, reader3 *bufio.Reader
 
 		It("read CSV file", func() {
 			machinesList, err = machineListReader(lc.machinesPath)
 			Expect(err).NotTo(HaveOccurred())
 		})
 
-		It("put que", func(ctx SpecContext) {
+		// ３サイクル分の処理をキューに積む
+		It("put que 1", func(ctx SpecContext) {
 			GinkgoWriter.Println(machinesList.machine)
 			lc.que.put(machinesList.machine)
-		}, SpecTimeout(time.Second))
+			fmt.Println("que len = ", lc.que.len())
+		}, SpecTimeout(3*time.Second))
+
+		It("put que 2", func(ctx SpecContext) {
+			GinkgoWriter.Println(machinesList.machine)
+			lc.que.put(machinesList.machine)
+			fmt.Println("que len = ", lc.que.len())
+		}, SpecTimeout(3*time.Second))
+
+		It("put que 3", func(ctx SpecContext) {
+			GinkgoWriter.Println(machinesList.machine)
+			lc.que.put(machinesList.machine)
+			fmt.Println("que len = ", lc.que.len())
+		}, SpecTimeout(3*time.Second))
 
 		// 複数のワーカーを起動
 		It("start three worker", func() {
@@ -125,47 +132,46 @@ var _ = Describe("Collecting by parallel workers", Ordered, func() {
 				go lc.worker(i)
 			}
 			// 起動待ち
-			time.Sleep(20 * time.Second)
+			time.Sleep(10 * time.Second)
 		})
 
-		///////////////////////////////////////////////////////////
-		It("put que", func(ctx SpecContext) {
-			GinkgoWriter.Println(machinesList.machine)
-			lc.que.put(machinesList.machine)
-		}, SpecTimeout(time.Second))
-
 		It("Check 1st reply from iDRAC #1", func() {
+			// ファイルが生成されるまで待つ
 			for {
 				file1, err = os.Open(path.Join(lc.testOut, serial1))
 				if errors.Is(err, os.ErrNotExist) {
 					time.Sleep(3 * time.Second)
 					continue
 				}
-				reader1 = bufio.NewReaderSize(file1, 4096)
-				stringJSON, _ := reader1.ReadString('\n')
-				fmt.Println("**** 11 stringJSON=", stringJSON)
-				json.Unmarshal([]byte(stringJSON), &rslt1)
-				GinkgoWriter.Println("------ ", string(rslt1.Serial))
-				GinkgoWriter.Println("------ ", string(rslt1.Id))
 				break
 			}
+
+			reader1 = bufio.NewReaderSize(file1, 4096)
+			stringJSON, _ := reader1.ReadString('\n')
+			GinkgoWriter.Println("**** 11 stringJSON=", stringJSON)
+
+			json.Unmarshal([]byte(stringJSON), &rslt1)
+			GinkgoWriter.Println("------ ", string(rslt1.Serial))
+			GinkgoWriter.Println("------ ", string(rslt1.Id))
 			Expect(rslt1.Serial).To(Equal(serial1))
 			Expect(rslt1.Id).To(Equal("1"))
 		})
 
 		It("Check 2nd reply from iDRAC #1", func() {
+			rslt1 = SystemEventLog{}
 			var stringJSON string
+
+			// ファイルが生成されるまで待つ
 			for {
 				stringJSON, err = reader1.ReadString('\n')
 				if err == io.EOF {
 					time.Sleep(1 * time.Second)
 					continue
-				} else {
-					break
 				}
+				break
 			}
 			fmt.Println("**** 12 stringJSON=", stringJSON)
-			rslt1 = SystemEventLog{}
+
 			json.Unmarshal([]byte(stringJSON), &rslt1)
 			GinkgoWriter.Println("------ ", string(rslt1.Serial))
 			GinkgoWriter.Println("------ ", string(rslt1.Id))
@@ -181,23 +187,35 @@ var _ = Describe("Collecting by parallel workers", Ordered, func() {
 					time.Sleep(3 * time.Second)
 					continue
 				}
-				reader2 = bufio.NewReaderSize(file2, 4096)
-				stringJSON, _ := reader2.ReadString('\n')
-				GinkgoWriter.Println("*1 stringJSON=", stringJSON)
-				json.Unmarshal([]byte(stringJSON), &rslt2)
-				GinkgoWriter.Println("------ ", string(rslt2.Serial))
-				GinkgoWriter.Println("------ ", string(rslt2.Id))
 				break
 			}
+			reader2 = bufio.NewReaderSize(file2, 4096)
+			stringJSON, _ := reader2.ReadString('\n')
+
+			GinkgoWriter.Println("*1 stringJSON=", stringJSON)
+			json.Unmarshal([]byte(stringJSON), &rslt2)
+			GinkgoWriter.Println("------ ", string(rslt2.Serial))
+			GinkgoWriter.Println("------ ", string(rslt2.Id))
+
 			Expect(rslt2.Serial).To(Equal(serial2))
 			Expect(rslt2.Id).To(Equal("1"))
 		})
 
 		It("Check 2nd reply from iDRAC #2", func() {
-			time.Sleep(3 * time.Second)
-			stringJSON, _ := reader2.ReadString('\n')
-			GinkgoWriter.Println("*2 stringJSON=", stringJSON)
+			var stringJSON string
 			rslt2 = SystemEventLog{}
+
+			// ファイルが追記されるまで待機
+			for {
+				stringJSON, err = reader2.ReadString('\n')
+				if err == io.EOF {
+					time.Sleep(1 * time.Second)
+					continue
+				}
+				break
+			}
+			GinkgoWriter.Println("*2 stringJSON=", stringJSON)
+
 			json.Unmarshal([]byte(stringJSON), &rslt2)
 			GinkgoWriter.Println("------ ", string(rslt2.Serial))
 			GinkgoWriter.Println("------ ", string(rslt2.Id))
@@ -206,10 +224,18 @@ var _ = Describe("Collecting by parallel workers", Ordered, func() {
 		})
 
 		It("Check 3rd reply from iDRAC #2", func() {
-			time.Sleep(3 * time.Second)
-			stringJSON, _ := reader2.ReadString('\n')
-			GinkgoWriter.Println("*3 stringJSON=", stringJSON)
+			var stringJSON string
 			rslt2 = SystemEventLog{}
+			for {
+				stringJSON, err = reader2.ReadString('\n')
+				if err == io.EOF {
+					time.Sleep(1 * time.Second)
+					continue
+				}
+				break
+			}
+			GinkgoWriter.Println("*3 stringJSON=", stringJSON)
+
 			json.Unmarshal([]byte(stringJSON), &rslt2)
 			GinkgoWriter.Println("------ ", string(rslt2.Serial))
 			GinkgoWriter.Println("------ ", string(rslt2.Id))
@@ -219,19 +245,18 @@ var _ = Describe("Collecting by parallel workers", Ordered, func() {
 
 		It("Check 4th reply from iDRAC #2", func() {
 			var stringJSON string
-			//time.Sleep(10 * time.Second)
+			rslt2 = SystemEventLog{}
+
 			for {
 				stringJSON, err = reader2.ReadString('\n')
 				if err == io.EOF {
 					time.Sleep(1 * time.Second)
 					continue
-				} else {
-					break
 				}
+				break
 			}
-			//stringJSON, _ := reader2.ReadString('\n')
 			GinkgoWriter.Println("*4 stringJSON=", stringJSON)
-			rslt2 = SystemEventLog{}
+
 			json.Unmarshal([]byte(stringJSON), &rslt2)
 			GinkgoWriter.Println("------ ", string(rslt2.Serial))
 			GinkgoWriter.Println("------ ", string(rslt2.Id))
@@ -239,101 +264,112 @@ var _ = Describe("Collecting by parallel workers", Ordered, func() {
 			Expect(rslt2.Id).To(Equal("4"))
 		})
 
-		/*
-			It("Check 1st reply from iDRAC #3", func() {
-				for {
-					file3, err = os.Open(path.Join(lc.testOut, serial3))
-					if errors.Is(err, os.ErrNotExist) {
-						time.Sleep(3 * time.Second)
-						continue
-					}
-					reader3 = bufio.NewReaderSize(file3, 4096)
-					stringJSON, _ := reader3.ReadString('\n')
-					GinkgoWriter.Println("*1 stringJSON=", stringJSON)
-					json.Unmarshal([]byte(stringJSON), &rslt3)
-					GinkgoWriter.Println("------ ", string(rslt3.Serial))
-					GinkgoWriter.Println("------ ", string(rslt3.Id))
-					break
+		It("Check 1st reply from iDRAC #3", func() {
+			for {
+				file3, err = os.Open(path.Join(lc.testOut, serial3))
+				if errors.Is(err, os.ErrNotExist) {
+					time.Sleep(3 * time.Second)
+					continue
 				}
-				Expect(rslt3.Serial).To(Equal(serial3))
-				Expect(rslt3.Id).To(Equal("1"))
-			})
+				break
+			}
+			reader3 = bufio.NewReaderSize(file3, 4096)
+			stringJSON, _ := reader3.ReadString('\n')
+			GinkgoWriter.Println("*1 stringJSON=", stringJSON)
 
-			It("Check 2nd reply from iDRAC #3", func() {
-				time.Sleep(3 * time.Second)
-				stringJSON, _ := reader3.ReadString('\n')
-				GinkgoWriter.Println("*2 stringJSON=", stringJSON)
-				rslt3 = SystemEventLog{}
-				json.Unmarshal([]byte(stringJSON), &rslt3)
-				GinkgoWriter.Println("------ ", string(rslt3.Serial))
-				GinkgoWriter.Println("------ ", string(rslt3.Id))
-				Expect(rslt3.Serial).To(Equal(serial3))
-				Expect(rslt3.Id).To(Equal("2"))
-			})
+			json.Unmarshal([]byte(stringJSON), &rslt3)
+			GinkgoWriter.Println("------ ", string(rslt3.Serial))
+			GinkgoWriter.Println("------ ", string(rslt3.Id))
 
-			It("Check 3rd reply from iDRAC #3", func() {
-				time.Sleep(3 * time.Second)
-				stringJSON, _ := reader3.ReadString('\n')
-				GinkgoWriter.Println("*3 stringJSON=", stringJSON)
-				rslt3 = SystemEventLog{}
-				json.Unmarshal([]byte(stringJSON), &rslt3)
-				GinkgoWriter.Println("------ ", string(rslt3.Serial))
-				GinkgoWriter.Println("------ ", string(rslt3.Id))
-				Expect(rslt3.Serial).To(Equal(serial3))
-				Expect(rslt3.Id).To(Equal("1"))
-			})
+			Expect(rslt3.Serial).To(Equal(serial3))
+			Expect(rslt3.Id).To(Equal("1"))
+		})
 
-			It("Check 4th reply from iDRAC #3", func() {
-				time.Sleep(3 * time.Second)
-				stringJSON, _ := reader3.ReadString('\n')
-				GinkgoWriter.Println("*4 stringJSON=", stringJSON)
-				rslt3 = SystemEventLog{}
-				json.Unmarshal([]byte(stringJSON), &rslt3)
-				GinkgoWriter.Println("------ ", string(rslt3.Serial))
-				GinkgoWriter.Println("------ ", string(rslt3.Id))
-				Expect(rslt3.Serial).To(Equal(serial3))
-				Expect(rslt3.Id).To(Equal("2"))
-			})
+		It("Check 2nd reply from iDRAC #3", func() {
+			rslt3 = SystemEventLog{}
+			var stringJSON string
 
-			It("Check 5th reply from iDRAC #3", func() {
-				var stringJSON string
-				for {
-					stringJSON, err = reader3.ReadString('\n')
-					if err == io.EOF {
-						time.Sleep(1 * time.Second)
-						continue
-					} else {
-						break
-					}
+			for {
+				stringJSON, err = reader3.ReadString('\n')
+				if err == io.EOF {
+					time.Sleep(1 * time.Second)
+					continue
 				}
-				//stringJSON, err := reader3.ReadString('\n')
-				//if err != nil {
-				//	fmt.Println(err)
-				//}
-				GinkgoWriter.Println("*5 stringJSON=", stringJSON)
-				rslt3 = SystemEventLog{}
-				err = json.Unmarshal([]byte(stringJSON), &rslt3)
-				if err != nil {
-					fmt.Println(err)
-				}
-				GinkgoWriter.Println("------ ", string(rslt3.Serial))
-				GinkgoWriter.Println("------ ", string(rslt3.Id))
-				Expect(rslt3.Serial).To(Equal(serial3))
-				Expect(rslt3.Id).To(Equal("3"))
-			})
+				break
+			}
+			GinkgoWriter.Println("*2 stringJSON=", stringJSON)
 
-				It("Check 5th reply from iDRAC #3", func() {
-					time.Sleep(20 * time.Second)
-					stringJSON, _ := reader3.ReadString('\n')
-					GinkgoWriter.Println("*5 stringJSON=", stringJSON)
-					rslt3 = SystemEventLog{}
-					json.Unmarshal([]byte(stringJSON), &rslt3)
-					GinkgoWriter.Println("------ ", string(rslt3.Serial))
-					GinkgoWriter.Println("------ ", string(rslt3.Id))
-					Expect(rslt3.Serial).To(Equal(serial3))
-					Expect(rslt3.Id).To(Equal("2"))
-				})
-		*/
+			json.Unmarshal([]byte(stringJSON), &rslt3)
+			GinkgoWriter.Println("------ ", string(rslt3.Serial))
+			GinkgoWriter.Println("------ ", string(rslt3.Id))
+
+			Expect(rslt3.Serial).To(Equal(serial3))
+			Expect(rslt3.Id).To(Equal("2"))
+		})
+
+		It("Check 3rd reply from iDRAC #3 after SEL cleanup", func() {
+			rslt3 = SystemEventLog{}
+			var stringJSON string
+
+			for {
+				stringJSON, err = reader3.ReadString('\n')
+				if err == io.EOF {
+					time.Sleep(1 * time.Second)
+					continue
+				}
+				break
+			}
+			GinkgoWriter.Println("*3 stringJSON=", stringJSON)
+			json.Unmarshal([]byte(stringJSON), &rslt3)
+
+			GinkgoWriter.Println("------ ", string(rslt3.Serial))
+			GinkgoWriter.Println("------ ", string(rslt3.Id))
+			Expect(rslt3.Serial).To(Equal(serial3))
+			Expect(rslt3.Id).To(Equal("1"))
+		})
+
+		It("Check 4th reply from iDRAC #3 after SEL cleanup", func() {
+			rslt3 = SystemEventLog{}
+			var stringJSON string
+
+			for {
+				stringJSON, err = reader3.ReadString('\n')
+				if err == io.EOF {
+					time.Sleep(1 * time.Second)
+					continue
+				}
+				break
+			}
+			GinkgoWriter.Println("*3 stringJSON=", stringJSON)
+			json.Unmarshal([]byte(stringJSON), &rslt3)
+
+			GinkgoWriter.Println("------ ", string(rslt3.Serial))
+			GinkgoWriter.Println("------ ", string(rslt3.Id))
+			Expect(rslt3.Serial).To(Equal(serial3))
+			Expect(rslt3.Id).To(Equal("2"))
+		})
+
+		It("Check 5th reply from iDRAC #3 after SEL cleanup", func() {
+			rslt3 = SystemEventLog{}
+			var stringJSON string
+
+			for {
+				stringJSON, err = reader3.ReadString('\n')
+				if err == io.EOF {
+					time.Sleep(1 * time.Second)
+					continue
+				}
+				break
+			}
+			GinkgoWriter.Println("*3 stringJSON=", stringJSON)
+			json.Unmarshal([]byte(stringJSON), &rslt3)
+
+			GinkgoWriter.Println("------ ", string(rslt3.Serial))
+			GinkgoWriter.Println("------ ", string(rslt3.Id))
+			Expect(rslt3.Serial).To(Equal(serial3))
+			Expect(rslt3.Id).To(Equal("3"))
+		})
+
 	})
 
 	AfterAll(func() {
@@ -341,5 +377,4 @@ var _ = Describe("Collecting by parallel workers", Ordered, func() {
 		lc.cancel()
 		time.Sleep(5 * time.Second)
 	})
-
 })
