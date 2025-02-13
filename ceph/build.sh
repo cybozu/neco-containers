8725e10a1d314a4b194a1166cf5631c4707856e1
Shinya Hayashi <shinya-hayashi@cybozu.co.jp>
2025-02-13 06:20:54 +0000

    ceph: update to v19.2.1
    Signed-off-by: Shinya Hayashi <shinya-hayashi@cybozu.co.jp>

--- ceph/build.sh
+++ ceph/build.sh
@@ -28,13 +28,6 @@ apt-get install -y libunwind-dev
 
 apt-get install -y curl
 
-# Ceph v18.2.4 has a bug that causes the ceph-volume command to fail on the OSD prepare pod.
-# This bug was caused by the wrong way of installing the Python package.
-# As a workaround, the following patch is applied ahead of upstream.
-# https://github.com/ceph/ceph/commit/729fd8e25ff2bfbcf99790d6cd08489d1c4e2ede
-# Apply the packing-1.patch(Commit 80edcd4) simply because the changes depend on it.
-patch -p1 < ${CEPH_DIR}/packing-1.patch
-patch -p1 < ${CEPH_DIR}/packing-2.patch
 ./install-deps.sh
 apt-get install -y python3-routes
 
@@ -44,10 +37,6 @@ apt-get install -y python3-routes
 # Build Ceph packages
 sed -i -e 's/WITH_CEPHFS_JAVA=ON/WITH_CEPHFS_JAVA=OFF/' debian/rules
 sed -i -e 's@usr/share/java/libcephfs-test.jar@@' debian/ceph-test.install
-# CMake in the self-build environment did not allow space-separated URLs.
-# As a workaround, the following patch is applied ahead of upstream.
-# https://github.com/ceph/ceph/commit/35435420781f84e9b71f72b10e6842a89c06de7f
-patch -p1 < ${CEPH_DIR}/boost-url.patch
 rm debian/libcephfs-java.jlibs debian/libcephfs-jni.install debian/ceph-mgr-dashboard*
 # To avoid OOM killer, use 10 parallelism instead of 20 (max vCPU).
 dpkg-buildpackage --build=binary -uc -us -j10
